df9415b723ae715337b57e333bff4d2fb8868cb2
zds <1030964891@qq.com>
2020-05-19 10:36:54 +0800

    喜欢功能

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
   Widget _buildSuggestions() {
     return new ListView.builder(
@@ -55,11 +56,25 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+      alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null,
+    ),
+    onTap: () {
+      setState(() {
+        if (alreadySaved) {
+          _saved.remove(pair);
+        } else {
+          _saved.add(pair);
+        }
+      });
+    },
     );
   }
 }
